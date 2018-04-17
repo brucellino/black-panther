e8282a28d78b8ca999c40525ebeab9594e34b636
Bruce Becker <brucellino@gmail.com>
2018-04-17 14:49:08 +0200

    Add rubocop

--- Gemfile
+++ Gemfile
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
-source "https://rubygems.org"
 
-# gem "rails"
+source 'https://rubygems.org'
 
 # Added at 2018-04-17 07:28:47 +0200 by becker:
-gem "httparty", "~> 0.16.2"
+gem 'httparty', '~> 0.16.2'
 
 # Added at 2018-04-17 07:29:12 +0200 by becker:
-gem "awesome_print", "~> 1.8"
+gem 'awesome_print', '~> 1.8'
 
 # Added at 2018-04-17 07:29:21 +0200 by becker:
-gem "json", "~> 2.1"
+gem 'json', '~> 2.1'
 
 # Added at 2018-04-17 08:31:16 +0200 by becker:
-gem "inspec", "~> 2.1"
+gem 'inspec', '~> 2.1'
+
+# Added at 2018-04-17 14:48:01 +0200 by becker:
+gem "rubocop", "~> 0.55.0"
---
+++ controls/black_panther.rb
@@ -0,0 +1,62 @@
+# copyright: 2018, The Authors
+
+require 'httparty'
+require 'json'
+require 'awesome_print'
+
+if ENV['GITHUB_TOKEN'].nil?
+  raise 'Please set the GITHUB_TOKEN environment variable'
+end
+if ENV['GITHUB_REPO'].nil?
+  raise 'Please set the GITHUB_REPO environment variable'
+end
+
+github_token = ENV['GITHUB_TOKEN']
+repo = ENV['GITHUB_REPO']
+
+options = {
+  headers: {
+    'Accept' => 'application/vnd.github.black-panther-preview+json',
+    'token' => github_token,
+    'User-Agent' => 'httparty'
+  }
+}
+url = "https://api.github.com/repos/#{repo}/community/profile"
+black_panther = HTTParty.get(url, options)
+File.open('black-panther.json', 'w') do |f|
+  f.write(black_panther)
+end
+
+control 'Bus Error' do
+  impact 0.5
+  title 'Documentation should be present'
+  desc 'Document everything'
+  describe json('black-panther.json') do
+    its('documentation') { should be }
+  end
+end
+
+# Community Power !
+control 'Power to the People' do # A unique ID for this control
+  impact 0.75 # The criticality, if this control fails.
+  title 'Black Panther ✊🏿'
+  desc 'Before you can Revolt, you Must be able to Read'
+  describe json('black-panther.json') do # The actual test
+    its('health_percentage') { should be > 75 }
+  end
+  describe json('black-panther.json') do
+    its(['files', 'contributing']) { should be }
+    its(['files', 'readme']) { should be }
+    its(['files', 'pull_request_template']) { should be }
+  end
+end
+
+control 'I fought the law, but the law won' do
+  impact 0.75
+  title 'Wynona Earp  👮 ‍♀️'
+  describe json('black-panther.json') do
+    its(['files', 'code_of_conduct']) { should be }
+    its(['files', 'license', 'key']) { should match '.*pache.*' }
+    its(['files', 'license', 'spdx_id']) { should cmp 'Apache-2.0' }
+  end
+end
